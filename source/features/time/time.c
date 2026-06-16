9255af5ad4d2d74cd7795f8930ae0b00aeb5c4ee
JV <jaiveer.chadda@gmail.com>
2026-06-17 02:18:14 +0530

    Added functionality to highlight the times of files that were changed this month and this year
    Also spent AGES on finding the right palette for all of them
    
    I'm still not 100% happy, but I think this'll do lol

--- source/features/time/time.c
+++ source/features/time/time.c
@@ -23,6 +23,8 @@
 
 time_t current_time;
 time_t diff_midn;	/// Number of seconds since midnight.
+time_t diff_month;	/// Number of seconds since midnight on the 1ˢᵗ of the month.
+time_t diff_year;	/// Number of seconds since midnight on the 1ˢᵗ of January this year.
 
 inline void initTime(void) {
 	current_time = time(NULL);
@@ -30,8 +32,11 @@ inline void initTime(void) {
 
 	diff_midn =
 		(cur_time_tm->tm_hour * HOUR) +
-		(cur_time_tm->tm_min  * MIN)  +
-		(cur_time_tm->tm_sec);
+		(cur_time_tm->tm_min  * MIN	) +
+		(cur_time_tm->tm_sec		);
+
+	diff_month	= diff_midn + (cur_time_tm->tm_mday * DAY);
+	diff_year	= diff_midn + (cur_time_tm->tm_yday * DAY);
 }
 
 inline void parseTime(char time_str[MAX_TIME_LEN], const time_t file_time, TimeColour *time_col) {
@@ -41,14 +46,16 @@ inline void parseTime(char time_str[MAX_TIME_LEN], const time_t file_time, TimeC
 
 	size_t b_writ = -1;	// (bytes written)
 
-	if		(t_diff <	  MIN)				{ SET_TIME_TEXT("Now"		); *time_col = TC_NOW	;}
-	else if	(t_diff < 2 * MIN)				{ SET_TIME_TEXT("1 min. ago"); *time_col = TC_1MIN	;}
-	else if	(t_diff < 3 * MIN)				{ SET_TIME_TEXT("2 mins ago"); *time_col = TC_2MIN	;}
-	else if	(t_diff < 4 * MIN)				{ SET_TIME_TEXT("3 mins ago"); *time_col = TC_3MIN	;}
-	else if	(t_diff < diff_midn			  )	{ SET_DATE_TEXT("Today"		); *time_col = TC_TODAY	;}
-	else if	(t_diff < diff_midn +	  DAY )	{ SET_DATE_TEXT("Yesterday"	); *time_col = TC_YESTD	;}
-	else if	(t_diff < diff_midn + 2 * DAY )	{ SET_DATE_TEXT("2 Days Ago"); *time_col = TC_2DAYS	;}
-	else								 	{ SET_DATE_TEXT( DATE_FMT	); *time_col = TC_OTHER ;}
+	if		(t_diff <	  MIN)				{ SET_TIME_TEXT("Now"		); *time_col = TC_NOW	 ; }
+	else if	(t_diff < 2 * MIN)				{ SET_TIME_TEXT("1 min. ago"); *time_col = TC_1MIN	 ; }
+	else if	(t_diff < 3 * MIN)				{ SET_TIME_TEXT("2 mins ago"); *time_col = TC_2MIN	 ; }
+	else if	(t_diff < 4 * MIN)				{ SET_TIME_TEXT("3 mins ago"); *time_col = TC_3MIN	 ; }
+	else if	(t_diff < diff_midn			 )	{ SET_DATE_TEXT("Today"		); *time_col = TC_TODAY	 ; }
+	else if	(t_diff < diff_midn +	  DAY)	{ SET_DATE_TEXT("Yesterday"	); *time_col = TC_YESTD	 ; }
+	else if	(t_diff < diff_midn + 2 * DAY)	{ SET_DATE_TEXT("2 Days Ago"); *time_col = TC_2DAYS	 ; }
+	else if	(t_diff < diff_month)			{ SET_DATE_TEXT(DATE_FMT	); *time_col = TC_THIS_MO; }
+	else if	(t_diff < diff_year	)			{ SET_DATE_TEXT(DATE_FMT	); *time_col = TC_THIS_YR; }
+	else								 	{ SET_DATE_TEXT(DATE_FMT	); *time_col = TC_OTHER	 ; }
 
 	if (!b_writ || file_time == 0) strcpy(time_str, TIME_ERR_STR);
 }
