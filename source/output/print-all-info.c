20de85cce676b4d31897bd5314201d396df399c7
JV <jaiveer.chadda@gmail.com>
2026-06-20 01:39:20 +0530

    Moved the header printing into its own file, and renamed `print-info` to `print-all-info`

--- source/output/print-all-info.c
+++ source/output/print-all-info.c
@@ -1,4 +1,4 @@
-/// @file info/print-info.c
+/// @file info/print-all-info.c
 
 #include <stdio.h>
 #include <string.h>
@@ -9,34 +9,6 @@
 #include "../features/mode/mode.h"
 
 #include "output.h"
-
-/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
-
-#define SHOULD_ALIGN_RIGHT(field) \
-	(strcmp(field##_TITLE, size_TITLE) == 0 || strcmp(field##_TITLE, time_str_TITLE) == 0)
-
-#define PRINT_HEADER(field)														\
-	if (do_##field)	{															\
-		printf(SHOULD_ALIGN_RIGHT(field) ? "%*s%s" : "%-*s%s",					\
-			(int)field_lengths.field + GRAPHICS_LEN - (int)strlen(FIELD_PAD),	\
-			HEADER_HL field##_TITLE HEADER_HL_OFF, FIELD_PAD					\
-		);																		\
-	}
-
-inline void printHeader(void) {
-	const int GRAPHICS_LEN = (int)strlen(HEADER_HL HEADER_HL_OFF FIELD_PAD);
-
-	PRINT_HEADER(inode)	;	PRINT_HEADER(dev_no)  ;
-	PRINT_HEADER(mode)	;	PRINT_HEADER(mode_str);
-	PRINT_HEADER(nlink)	;
-	PRINT_HEADER(size)	;	PRINT_HEADER(size_str);
-	PRINT_HEADER(uid)	;	PRINT_HEADER(usr_name);
-	PRINT_HEADER(gid)	;	PRINT_HEADER(grp_name);
-	PRINT_HEADER(flags)	;	PRINT_HEADER(flag_str);
-	PRINT_HEADER(time)	;	PRINT_HEADER(time_str);
-
-	puts(" " HEADER_HL name_TITLE HEADER_HL_OFF);
-}
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
---
+++ source/output/print-header.c
@@ -0,0 +1,35 @@
+/// @file output/print-header.c
+
+#include <stdio.h>
+#include <string.h>
+
+#include "../options/options.h"
+#include "../graphics/graphics.h"
+
+#include "output.h"
+
+#define SHOULD_ALIGN_RIGHT(field) \
+	(strcmp(field##_TITLE, size_TITLE) == 0 || strcmp(field##_TITLE, time_str_TITLE) == 0)
+
+#define PRINT_HEADER(field)														\
+	if (do_##field)	{															\
+		printf(SHOULD_ALIGN_RIGHT(field) ? "%*s%s" : "%-*s%s",					\
+			(int)field_lengths.field + GRAPHICS_LEN - (int)strlen(FIELD_PAD),	\
+			HEADER_HL field##_TITLE HEADER_HL_OFF, FIELD_PAD					\
+		);																		\
+	}
+
+inline void printHeader(void) {
+	const int GRAPHICS_LEN = (int)strlen(HEADER_HL HEADER_HL_OFF FIELD_PAD);
+
+	PRINT_HEADER(inode)	;	PRINT_HEADER(dev_no)  ;
+	PRINT_HEADER(mode)	;	PRINT_HEADER(mode_str);
+	PRINT_HEADER(nlink)	;
+	PRINT_HEADER(size)	;	PRINT_HEADER(size_str);
+	PRINT_HEADER(uid)	;	PRINT_HEADER(usr_name);
+	PRINT_HEADER(gid)	;	PRINT_HEADER(grp_name);
+	PRINT_HEADER(flags)	;	PRINT_HEADER(flag_str);
+	PRINT_HEADER(time)	;	PRINT_HEADER(time_str);
+
+	puts(" " HEADER_HL name_TITLE HEADER_HL_OFF);
+}
