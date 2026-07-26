ec2abd8f051931622d47b4ca3db305556bb81079
JV <jaiveer.chadda@gmail.com>
2026-07-26 12:53:17 +0200

    Cleaned up the symlink-finding function

--- source/features/links/symlink.c
+++ source/features/links/symlink.c
@@ -5,36 +5,49 @@
 #include <unistd.h>
 #include <string.h>
 
-#include "../../model/stat-model.h"
 #include "../mode/mode.h"
+#include "../../model/stat-model.h"
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
 link_t getLink(const path_t link_path) {
 	path_t link;
 
-	const ssize_t nbytes = readlink(link_path, link, sizeof(path_t));
-	link[nbytes] = '\0';
+	const ssize_t path_len = readlink(link_path, link, sizeof(path_t));
+	link[path_len] = '\0';
 
-	const char *HOME = getenv("HOME");
-
-	const int home_len = strlen(HOME);
-	const int path_len = strlen(link);
-
-	int bytes_written;
+	/* ————————————————————————————————————————————————————— */
 
 	// this memory is freed once the link is printed (in `printSymlink()`)
 	link_t link_str = malloc(sizeof(char) * MAX_PATH_LEN);
-	if (link_str == NULL) { perror(ERROR "malloc:link_str"); exit(EXIT_FAILURE); }
+
+	if (link_str == NULL) {
+		perror(ERROR "malloc:link_str");
+		exit(EXIT_FAILURE);
+	}
+
+	/* ————————————————————————————————————————————————————— */
+
+	const char *HOME = getenv("HOME");
+	const int home_len = strlen(HOME);
+
+	int bytes_written;
 
 	if (HOME != NULL
 		&& home_len > 0
 		&& home_len < path_len
 		&& strncmp(HOME, link, home_len) == 0
-	) { bytes_written = sprintf(link_str, "~%s", link + home_len);
+	) {
+		bytes_written = sprintf(link_str, "~%s", link + home_len);
 	} else {
 		bytes_written = sprintf(link_str, "%s", link);
 	}
 
 	link_str[bytes_written] = '\0';
 
+	/* ————————————————————————————————————————————————————— */
+
 	return link_str;
 }
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
