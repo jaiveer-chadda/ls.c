ea73b6ce2905f7e7b0e78f98b07f076309df6465
JV <jaiveer.chadda@gmail.com>
2026-08-18 16:22:01 +0100

    build: Completely rework `build.zsh`
    It was getting kinda unruly, and if I wanted to add/remove things from it going forward then it was gonna start getting pretty tough tbh
    
    There's still a bit to do, but this version of the file is mostly done

--- build.zsh
+++ build.zsh
@@ -10,70 +10,109 @@ if { false; } {
     -Wno-variadic-macro-arguments-omitted  \
     -L"$( brew --prefix libmagic )"lib     \
     -I"$( brew --prefix libmagic )"include \
-    -I/Users/jv/dev/C/ls.c/source          \
+    -I$HOME/dev/C/ls.c/source              \
     -lmagic -framework CoreFoundation      \
     -fsanitize=address,undefined           \
-    --output "$_root/out/lk"               \
-    "$_root/source/"**/*.c                 \
-      && "$_root/out/lk" --clear
+    "$root/source/"**/*.c                  \
+    --output "$root/out/lk"                \
+      &&     "$root/out/lk" --clear        \
+      && cp  "$root/out/lk" "$HOME/bin/lk"
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
 function -- () {
 
-  readonly _root="${${(%):-%x}:a:h}"
-
-  readonly CC=clang
-  readonly -a CFLAGS=( O0 )
+  # equivalent to running `dirname` on this file's path (w/o resolving links)
+  local -r _proj_root="${${(%):-%x}:a:h}"
 
   # ————————————————————————————————————————————————————————————————————————— #
 
-  local -a DEFINITIONS=()
-  if [[ "$1" == (--|)debug ]] DEFINITIONS+=( DEBUG_MODE ) && shift
+  local -r CC='clang'
+  local -ra CFLAGS=( O0 )
 
-  readonly -a WARNINGS=( all extra pedantic )
-  readonly -a NO_WARN=(
+  # ————————————————————————————————————————————————————————————————————————— #
+
+  # if the `--debug` flag is passed to this script, then define the
+  #  `DEBUG_MODE` macro for the compiler to build with debug enabled
+  local mode
+  if [[ "$1" == --debug ]] mode=debug && shift
+
+  local -a DEFINITIONS
+  if [[ "$mode" == debug ]] DEFINITIONS+=( DEBUG_MODE )
+
+  # ————————————————————————————————————————————————————————————————————————— #
+
+  # all `-W...` warnings to enable
+  local -a WARNINGS=( all extra pedantic )
+
+  # all `-W-no-...` warnings to disable
+  local -ra NO_WARN=(
+    # stops warnings about using `sprintf` instead of the preferred `snprintf`
     deprecated-declarations
+    # stops a warning about using a macro that passes 0 inputs to __VA_ARGS__
+    #  however, this is needed for the `OPTION_IS()` macro in `options.c`
     variadic-macro-arguments-omitted
   )
 
-  # ————————————————————————————————————————————————————————————————————————— #
-
-  readonly TARGET="$_root/out/lk"
-  readonly -a CMD=( "$TARGET" --clear )
-
-  # ———————————————————————————————————————————————————— #
-
-  readonly -a SOURCE_FILES=( "$_root/source/"**/*.c )
-
-  # ———————————————————————————————————————————————————— #
-
-  readonly -a FRAMEWORKS=( -framework CoreFoundation )
-  readonly -a _sanitise=( address undefined )
-
-  # find where the `libmagic` library is stored, and pass it to the linker
-  readonly _brew_prefix="$( brew --prefix libmagic )"
-  readonly -a INCLUDES=( "$_brew_prefix/include" "$_root/source" )
-  readonly -a  LDFLAGS=( "L$_brew_prefix/lib" fsanitize=${(j:,:)_sanitise} )
-  readonly -a   LDLIBS=( magic )
+  WARNINGS+=( "no-${(@)^NO_WARN}" )
 
   # ————————————————————————————————————————————————————————————————————————— #
 
-  readonly -a ALL_BUILD_ARGS=(
-    "-W-no-${(@)^NO_WARN}"
-    "-W${(@)^WARNINGS}"
-    "-D${(@)^DEFINITIONS}"
-    "-W${(@)^WARNINGS}"
-    "-I${(@)^INCLUDES}"
-    "-l${(@)^LDLIBS}"
-    "-${(@)^LDFLAGS}"
-    "-${(@)^CFLAGS}"
-    "${(@)FRAMEWORKS}"
-    "${(@)SOURCE_FILES}"
-  )
+  # location of the outputted binary
+  local -r TARGET="$_proj_root/out/lk"
+  local -r COPY_TO="$_proj_root/out/lk"
+  # the command that should be run after compilation & linking
+  local -ra CMD=( "$TARGET" --clear )
 
-  echo "$CC" "${(@)ALL_BUILD_ARGS}" --output "$TARGET" && "${(@)CMD}" "$@"
+  # ———————————————————————————————————————————————————— #
+
+  # an array of all the program's source files
+  local -ra SOURCE_FILES=( "$_proj_root/source/"**/*.c )
+
+  # ———————————————————————————————————————————————————— #
+
+  # find where the `libmagic` library is stored, to be passed to the linker
+  local -r _lmagic_prefix="$( brew --prefix libmagic )"
+
+  local -ra FRAMEWORKS=( CoreFoundation )
+  local -ra   LIBPATHS=( "$_lmagic_prefix/lib" )
+  local -ra   INCLUDES=( "$_lmagic_prefix/include" "$_proj_root/source" )
+  local -ra   SANITISE=( address undefined )
+  local -ra     LDLIBS=( magic )
+
+  # ————————————————————————————————————————————————————————————————————————— #
+
+  # pack the build args into an array, adding each of their relevant prefixes,
+  #  and making sure not to add any empty arrays
+  local -a BUILD_ARGS
+
+  if (( $#CFLAGS      )) BUILD_ARGS+=(  "-${(@)^CFLAGS}"      )
+  if (( $#WARNINGS    )) BUILD_ARGS+=( "-W${(@)^WARNINGS}"    )
+  if (( $#DEFINITIONS )) BUILD_ARGS+=( "-D${(@)^DEFINITIONS}" )
+  if (( $#INCLUDES    )) BUILD_ARGS+=( "-I${(@)^INCLUDES}"    )
+  if (( $#LIBPATHS    )) BUILD_ARGS+=( "-L${(@)^LIBPATHS}"    )
+  if (( $#LDLIBS      )) BUILD_ARGS+=( "-l${(@)^LDLIBS}"      )
+  if (( $#FRAMEWORKS  )) BUILD_ARGS+=( "-framework ${(@)^FRAMEWORKS}" )
+  if (( $#SANITISE    )) BUILD_ARGS+=( "-fsanitize=${(j:,:)SANITISE}" )
+
+  # always add the target and source files
+  BUILD_ARGS+=( --output "$TARGET" -- )
+  BUILD_ARGS+=( "${(@)SOURCE_FILES}"  )
+
+  # echo "$CC" "${(@z)BUILD_ARGS}" '\ \n\t' \
+  #   '&&' "${(@)CMD}" "$@"         '\ \n\t' \
+  #   '&&' cp "$TARGET" "$HOME/bin/${TARGET##*/}" $'\e[m'
+
+  # pass that array of arguments to the compiler
+  # then, if successful, execute the program
+  # and if that _also_ works, make a copy of the binary available in `~/bin`
+  "$CC" "${(@z)BUILD_ARGS}" \
+    &&  "${(@)CMD}" "$@"     \
+    && cp "$TARGET" "$HOME/bin/${TARGET##*/}"
+
 } "$@"
 
-# spell:ignoreRegExp /(?<!-)-\w+/g
+# ——————————————————————————————————————————————————————————————————————————— #
+
+# spell:ignoreRegExp /(?<!-)[-_]\w+/g
