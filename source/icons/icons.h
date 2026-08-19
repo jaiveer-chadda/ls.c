fc3a7c9ce9d6e2f527b951755832ba0cbd4fd4fe
JV <jaiveer.chadda@gmail.com>
2026-08-19 15:45:43 +0100

    refactor(icons): Add an acknowledgements section to `icons.h`.

--- source/icons/icons.h
+++ source/icons/icons.h
@@ -1,4 +1,19 @@
-/// @file icons/icons.h
+/**
+ * @file icons/icons.h
+ *
+ * ACKNOWLEDGEMENTS
+ * ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+ *	- With great thanks to the [eza project](https://github.com/eza-community/eza), from whom many of the icon
+ *	  definitions and mappings have been copied verbatim.
+ *		- See the following file for the original source (accessed at commit 4a7bfa58fc864bed516678a8aa35fe3793f3b4a5):
+ *		- <https://github.com/eza-community/eza/blob/main/src/output/icons.rs>
+ *
+ *	- Thanks in particular to [Robert Minsk](https://github.com/cfxegbert), who seems to have created most of the
+ *	  icon mappings in eza's original `icons.rs`.
+ *
+ *	- The eza project is [licenced](https://github.com/eza-community/eza/blob/main/LICENSE.txt) under the
+ *	  [European Union Public License v.1.2](https://eupl.eu/1.2/en/).
+ */
 
 #include <stddef.h> // needed for `wchar_t`
 
