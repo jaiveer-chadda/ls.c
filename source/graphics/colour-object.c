e3ef7f7255cf3b6837a83169a40103b1bb689bb3
JV <jaiveer.chadda@gmail.com>
2026-08-26 17:22:45 +0100

    refactor(graphics): Simplify (ironically) the `SIMPLIFY_ANSI` section

--- source/graphics/colour-object.c
+++ source/graphics/colour-object.c
@@ -1,20 +1,33 @@
 /// @file graphics/colour-object.c
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
 #include "colour-object.h"
 
-typedef struct {
-	style_t style;
-	colour_t fg, bg;
-} Colour_;
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
 #define COLOUR_NONE		((Colour){0})
-#define DARK_MODE_INIT	((Colour){ .style = G_NONE						 , .fg = G_WHITE, .bg = G_BLACK })
-#define LIGHT_MODE_INIT	((Colour){ .style = G_NONE						 , .fg = G_WHITE, .bg = G_BLACK })
-#define test_1			((Colour){ .style = G_BOLD | G_UNDER | G_NO_RESET, .fg = G_BLACK, .bg = G_BLUE })
+#define DARK_MODE_INIT	((Colour){ .style = G_NONE, .fg = G_WHITE, .bg = G_BLACK })
+#define LIGHT_MODE_INIT	((Colour){ .style = G_NONE, .fg = G_WHITE, .bg = G_BLACK })
+
+/* —————————————————————————————————————————————————————————————————— */
+
+#define fg_ANSI_CODE 3
+#define bg_ANSI_CODE 4
+
+#define SIMPLIFY_ANSI(fgbg) do {																	\
+	if		(col.fgbg == prev.fgbg) { /* do nothing - don't add anything to the output */ }			\
+	else if	(col.fgbg <=  7) sprintf(fgbg##_str, "%d%hhu"	 , fgbg##_ANSI_CODE		, col.fgbg);	\
+	else if (col.fgbg <= 15) sprintf(fgbg##_str, "%d%hhu"	 , fgbg##_ANSI_CODE + 6	, col.fgbg - 8);\
+	else					 sprintf(fgbg##_str, "%d8;5;%hhu", fgbg##_ANSI_CODE		, col.fgbg);	\
+} while (0)
+
+/* —————————————————————————————————————————————————————————————————— */
+
+#define test_1 ((Colour){ .style = G_BOLD | G_UNDER | G_NO_RESET, .fg = 125, .bg = G_BLUE })
+#define test_2 ((Colour){ .style = G_DIM  | G_UNDER				, .fg = 125, .bg = G_RED })
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
@@ -42,8 +55,6 @@ static inline char *stylelookup(uint16_t style) {
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
 int colprint(const Colour col) {
-	printf("st : %#x \nfg : %hhu\nbg : %hhu\n\n", col.style, col.fg, col.bg);
-
 	// I'm making the assumption that if someone wants a black foreground, black background, and no style,
 	//	then they want the colour to be reset.
 	if (col.style + col.fg + col.bg == 0) { prev = col; return 0; }
@@ -66,22 +77,13 @@ int colprint(const Colour col) {
 
 	/* —————————————————————————————————————————————————————————————————— */
 
-	if		(col.fg == prev.fg) { /* do nothing - don't add anything to the output */ }
-	else if	(col.fg <=	7)	sprintf(fg_str, 	"3%hhu;", col.fg);
-	else if (col.fg <= 15)	sprintf(fg_str, 	"9%hhu;", col.fg - 8);
-	else					sprintf(fg_str, "38;5;%hhu;", col.fg);
-	
-	/* —————————————————————————————————————————————————————————————————— */
-
-	if		(col.bg == prev.bg) { /* do nothing - don't add anything to the output */ }
-	else if	(col.bg <=	7)	sprintf(bg_str, 	"4%hhum", col.bg);
-	else if (col.bg <= 15)	sprintf(bg_str,    "10%hhum", col.bg - 8);
-	else					sprintf(bg_str, "48;5;%hhum", col.bg);
+	SIMPLIFY_ANSI(fg);
+	SIMPLIFY_ANSI(bg);
 
 	/* —————————————————————————————————————————————————————————————————— */
 
-	printf("\\e[" "%s" "%s%s%s" "%s", do_reset ? ";" : "", style, fg_str, bg_str, "\n");
-	printf("\33[" "%s" "%s%s%s" "%s", do_reset ? ";" : "", style, fg_str, bg_str, "[lorem ipsum dolor]\33[m\n");
+	printf("\\e[" "%s" "%s%s;%sm" "%s", do_reset ? ";" : "", style, fg_str, bg_str, "\n");
+	printf("\33[" "%s" "%s%s;%sm" "%s", do_reset ? ";" : "", style, fg_str, bg_str, "[lorem ipsum dolor]\33[m\n");
 
 	/* —————————————————————————————————————————————————————————————————— */
 
@@ -93,8 +95,10 @@ int colprint(const Colour col) {
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
 int main(const int argc, const char* argv[]) {
-	colprint(test_1);
-	return EXIT_SUCCESS;
+	colprint(test_1); putchar('\n');
+	colprint(test_2); putchar('\n');
+
+	return 0;
 }
 
 // /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
