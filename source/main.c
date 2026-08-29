49b2d7e3421a68cf237189edc82e8b7664c5e1f9
JV <jaiveer.chadda@gmail.com>
2026-08-29 19:48:50 +0400

    feat: Begin creating the main processing loop, and implement the memory allocation steps for the `FileStat` and `stat` structs

--- source/main.c
+++ source/main.c
@@ -32,7 +32,7 @@ const char *argv0;
 int main(const int argc, char *argv[]) {
 	initDebugging(argv);
 
-	// Set the locale to the system default (it'll check the env vars)
+	// set the locale to the system default (it'll check the env vars)
 	//	(this is to ensure that multibyte characters can be printed as file icons)
 	const char *locale = setlocale(LC_ALL, "");
 	if(!strends(locale, "UTF-8")) debug(WARNING, "Non-UTF-8 locale - locale is '%s'", locale);
@@ -41,7 +41,7 @@ int main(const int argc, char *argv[]) {
 		
 	/* —— Parse User Options ————————————————————————————————————————————————————————————————————— */
 
-	// Parse the user's inputted options, and find where the options end (& where the files start)
+	// parse the user's inputted options, and find where the options end (& where the files start)
 	//	e.g. if the program is run as `lk --clear --sort name ~/.config/options`, then `files_start` will be 4
 	const int opt_count = setOptions(argc, (const char**)argv);
 
@@ -53,74 +53,69 @@ int main(const int argc, char *argv[]) {
 
 	/// The raw string paths inputted by the user.
 	char **file_paths = argv + opt_count;
-	// If there were no paths entered, then assume the user inputted the path `.`
+	// if there were no paths entered, then assume the user inputted the path `.`
 	if (opt_count >= argc) file_paths[0] = DOTDIR;
 
 	/* —— Find Target Files/Dirs ————————————————————————————————————————————————————————————————— */
 
+	/// Whether the user inputted at least one valid input into the function.
+	bool any_valid_input = false;
+
+	/// An array of pointers to FileStat objects, each representing the inputted files/dirs.
+	FileStat *fs_input_arr[file_count];
+
 	for (int i = 0; i < file_count; i++) {
 		const char *path = file_paths[i];
+		struct stat file_stat;
+
 		printf("%d: %s\n", i, path);
 
-		struct stat file_stat;
-		stat(path, &file_stat);
+		/* ———————————————————————————————————————————————————————————— */
 
+		// firstly, try to `stat` the input path
 		if (stat(path, &file_stat) == -1) {
+			// if it fails, print an error and move onto the next file
 			const int stat_errno = errno;
-			if (stat_errno == ENOENT) { /* handle */ };
+			if (stat_errno == ENOENT) { /* handle */ }
 
 			fprintf(stderr, "%s: %s: %s\n", argv0, path, strerror(stat_errno));
+
+			// set the pointer to this input to NULL, so we know not to process it later
+			fs_input_arr[i] = NULL;
 			continue;
 		}
+
+		// file was `stat`ted successfully - note down that we've had at least 1 valid input
+		any_valid_input = true;
+
+		/* ———————————————————————————————————————————————————————————— */
+
+		// since we successfully got the `stat` information, we can start building the `FileStat` object
+
+		// allocate memory for this file's `FileStat` object
+		FileStat *p_fsobj = emalloc(sizeof(FileStat));
+
+		// then zero the memory
+		*p_fsobj = (FileStat){0};
+		// and add its pointer to the input array
+		fs_input_arr[i] = p_fsobj;
+
+		/* ———————————————————————————————————————————————————————————— */
+
+		// allocate memory for the `stat` object that will be pointed to by `FileStat::s`
+		struct stat *p_stat = emalloc(sizeof(struct stat));
+
+		// copy `file_stat` from the stack into the newly-allocated heap memory,
+		//	and then assign the pointer to that heap memory to `FileStat::s`
+		p_fsobj->s = memcpy(p_stat, &file_stat, sizeof(struct stat));
+
+		/* ———————————————————————————————————————————————————————————— */
 
 		if (S_ISDIR(file_stat.st_mode)) {}
 	}
 
-	// /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
-	// /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
-	// /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
-	//
-	// /// Whether the user inputted at least one valid input into the function.
-	// bool has_any_valid_input = false;
-	//
-	// // Get a `DIR` pointer for each path passed in to the function
-	// // (`DIR` being a "structure describing an open directory")
-	// DIR *input_dirs[input_count];
-	//
-	// for (int i = 0; i < input_count; i++) {
-	// 	input_dirs[i] = opendir(input_paths[i]);
-	// 	const int opendir_errno = errno;
-	//
-	// 	// If we couldn't open the directory (usually cos it doesn't exist or
-	// 	//	we don't have permissions for it), print an error
-	// 	if (input_dirs[i] == NULL) {
-	// 		fprintf(stderr, "%s: %s: %s\n", argv0, input_paths[i], strerror(opendir_errno));
-	// 	} else {
-	// 		// If at least one inputted directory is valid, then make sure we continue
-	// 		has_any_valid_input = true;
-	// 	}
-	// }
-	//
-	// // If none the inputted directories are valid, exit with failure
-	// if (!has_any_valid_input) return EXIT_FAILURE;
-	//
-	// /* —— Get Current Time ——————————————————————————————————————————————————————————————————————— */
-	//
-	// // Find the current time and make it available globally
-	// initTime();
-	//
-	// /* —— Process All Directories ———————————————————————————————————————————————————————————————— */
-	//
-	// for (int i = 0; i < input_count; i++) {
-	// 	// Don't process invalid directories
-	// 	if (input_dirs[i] == NULL) continue;
-	//
-	// 	// Do the processing & print the details for each directory inputted
-	// 	processDirectory(input_paths[i], input_dirs[i], do_free_path_0, i == 0);
-	//
-	// 	// Print a newline between each directory listing (after each dir except the last)
-	// 	if (i != input_count - 1) putchar('\n');
-	// }
+	// if none of the inputted directories were valid, exit with failure
+	if (!any_valid_input) return EXIT_FAILURE;
 
 	return EXIT_SUCCESS;
 }
@@ -128,14 +123,10 @@ int main(const int argc, char *argv[]) {
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
 static inline const char *getArgv0(const int argc, char *restrict argv[]) {
-	if ((argc < 1) || (argv[0] == NULL) || (argv[0][0] == '\0')) {
-		return PROGRAM_NAME;
-	}
+	if ((argc < 1) || (argv[0] == NULL) || (argv[0][0] == '\0')) return PROGRAM_NAME;
 
 	const char *basename = strrchr(argv[0], '/');
-	if (basename != NULL) {
-		return basename + 1;
-	}
+	if (basename != NULL) return basename + 1;
 
 	return argv[0];
 }
