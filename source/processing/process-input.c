c000a5c578d16a5cb90a2be51b5e45b52b0517c4
JV <jaiveer.chadda@gmail.com>
2026-08-31 10:56:28 +0400

    refactor: Continue to extract component parts of the input processing into separate functions

--- source/processing/process-input.c
+++ source/processing/process-input.c
@@ -28,7 +28,7 @@ static inline void processChild(
 	const char *const dirpath,
 	const int16_t dirpath_len
 ) {
-	/* —— – basic child info (dirent) ——————————————————————————————— */
+	/* —— basic child info (dirent) ——————————————————————————————— */
 
 	// copy the info from `dirent` over to the child's `FileStat` object
 	*pfs_child = (FileStat){
@@ -43,7 +43,7 @@ static inline void processChild(
 	//	we're using `memcpy` and not `strcpy` since we already know how long the string is
 	memcpy(pfs_child->name, pd_child->d_name, pd_child->d_namlen + 1);
 
-	/* —— – get path to child ——————————————————————————————————————— */
+	/* —— get path to child ——————————————————————————————————————— */
 
 	// make sure that the resultant child path won't be too long once we create it
 	if ((size_t)(dirpath_len + 1 + pfs_child->name_len) >= sizeof(path_t)) {
@@ -62,7 +62,7 @@ static inline void processChild(
 	child_path[dirpath_len] = '/'; // add the path separator
 	memcpy(child_path + 1 + dirpath_len, pfs_child->name, pfs_child->name_len + 1); // +1 for '\0' this time
 
-	/* —— – `stat` child file ——————————————————————————————————————— */
+	/* —— `stat` child file ——————————————————————————————————————— */
 
 	// run `lstat` on the path
 	if (lstat(child_path, pfs_child->s) == -1) {
@@ -79,9 +79,92 @@ static inline void processChild(
 		return;
 	}
 
-	/* ———————————————————————————————————————————————————————————— */
-
 	printf("\t%s/%s\n", dirpath, pfs_child->name);
+}
+
+/* ── ── processDir ── ───────────────────────────────────────────────────────────────────────────────────────────── */
+
+static inline FileStat *processDir(char *const dirpath, FileStat *p_fsobj) {
+	// firstly, open the directory and get a pointer to a `DIR` object
+	//	note: we can't get any info from `DIR`, it's use is to be passed into other functions
+	DIR *p_dir = opendir(dirpath);
+
+	// if we can't open the directory, note that there aren't any children, and move on
+	if (p_dir == NULL) {
+		// set the number of children to -1, so we know the difference between having 0 children,
+		//	and not being able to open the directory
+		p_fsobj->f->child_count = -1;
+
+		printError(dirpath, strerror(errno));
+
+		// and make sure to close the dir - memory leaks!!
+		closedir(p_dir);
+		return p_fsobj;
+	}
+
+	// bite the bullet and use `strlen` to calculate the dir's length now,
+	//	bc we're going to need it to get the path to the children in a moment
+	const int16_t dirpath_len = (int16_t)strlen(dirpath);
+	p_fsobj->name_len = dirpath_len;
+
+	// allocate some memory for an arbitrary number of children, with the intention that
+	//	we'll realloc if we need more memory later
+	int32_t child_alloc_count = INIT_CHILD_COUNT;
+	FileStat *children = ecalloc(child_alloc_count, sizeof(FileStat));
+
+	/* —— For Each Child in Dir ——————————————————————————————————— */
+
+	// iterate through the directory until you run out of children (or there's an error)
+	const struct dirent *pd_child;
+	while (( pd_child = readdir(p_dir) ) != NULL) {
+		// if this child has the name ".", then it's not a child, but the dir itself
+		if (strcmp(pd_child->d_name, DOTDIR) == 0) {
+			// get the few pieces of information that we care about from the `dirent` object
+			// note: we're not keeping `d_name` or `d_namlen`, since these would just
+			//	return "." and 1 respectively, which isn't much help to us
+			p_fsobj->type = pd_child->d_type;
+			p_fsobj->inum = pd_child->d_ino;
+
+			continue; // continue - the rest of the information we need is already in `p_fsobj->s`
+		}
+
+		// with the output structure I'm building, I don't think it makes sense to show `..`
+		//	if I want to add an option to keep it later, I can just add it to this condition
+		if (strcmp(pd_child->d_name, "..") == 0) continue;
+		// note: when I add the `-a` and `-A` options later, this is where I'll add a check for them
+
+		/* —— alloc mem for children —————————————————————————————————— */
+
+		if (p_fsobj->f->child_count >= child_alloc_count) {
+			const int32_t old_alloc_count = child_alloc_count; // save the old capacity before multiplying
+
+			// if the number of children (before we increment it) is more than we have space for,
+			//	then increase the number of children we have space for, by a factor of 1.5.
+			// 1.5 is the optimal multiplier to make sure we don't over-allocate memory, but also so that
+			//	we aren't constantly allocating it either
+			MULT_BY_1_5(child_alloc_count);
+
+			// re-allocate the memory. note that `[e]realloc` will free the original pointer and hard exit if it
+			//	fails, so we don't have to worry about memory leaks by overwriting the original pointer
+			children = erealloc(children, sizeof(FileStat) * child_alloc_count);
+
+			// zero out the new memory that we were just allocated
+			//	this isn't strictly necessary, but it could prove rly useful for debugging
+			//	(I might remove it later if I don't think it's needed)
+			memset(children + old_alloc_count, 0, sizeof(FileStat) * (child_alloc_count - old_alloc_count));
+		}
+
+		// find the position where the child's `FileStat` object will start
+		FileStat *pfs_child = children + p_fsobj->f->child_count;
+
+		// only increment the child count now that we've used it as an index to calculate where the child should be
+		p_fsobj->f->child_count++;
+
+		processChild(pfs_child, pd_child, dirpath, dirpath_len);
+	}
+
+	closedir(p_dir);
+	return p_fsobj;
 }
 
 /* ── ── processInput ── ─────────────────────────────────────────────────────────────────────────────────────────── */
@@ -139,89 +222,10 @@ FileStat *processInput(char *path) {
 	// if the input was a file (i.e. not a dir), then there's nothing else to do at this stage
 	if (!S_ISDIR(file_stat.st_mode) || DIRS_AS_FILES()) return p_fsobj;
 
-	/* —— Setup for Input Dirs ——————————————————————————————————————————————————————————————————— */
-	// if the input was a directory, however, we need to find its children
+	/* —— Process Directories ———————————————————————————————————————————————————————————————————— */
 
-	char *const dirpath = path; // (changing the name of the variable to ease legibility)
-
-	// firstly, open the directory and get a pointer to a `DIR` object
-	//	note: we can't get any info from `DIR`, it's use is to be passed into other functions
-	DIR *p_dir = opendir(dirpath);
-
-	// if we can't open the directory, note that there aren't any children, and move on
-	if (p_dir == NULL) {
-		// set the number of children to -1, so we know the difference between having 0 children,
-		//	and not being able to open the directory
-		p_fsobj->f->child_count = -1;
-
-		printError(path, strerror(errno));
-		return p_fsobj;
-	}
-
-	// bite the bullet and use `strlen` to calculate the dir's length now,
-	//	bc we're going to need it to get the path to the children in a moment
-	const int16_t dirpath_len = (int16_t)strlen(dirpath);
-	p_fsobj->name_len = dirpath_len;
-
-	// allocate some memory for an arbitrary number of children, with the intention that
-	//	we'll realloc if we need more memory later
-	int32_t child_alloc_count = INIT_CHILD_COUNT;
-	FileStat *children = ecalloc(child_alloc_count, sizeof(FileStat));
-
-	/* —— For Each Child in Dir ——————————————————————————————————— */
-
-	// iterate through the directory until you run out of children (or there's an error)
-	const struct dirent *pd_child;
-	while (( pd_child = readdir(p_dir) ) != NULL) {
-		// if this child has the name ".", then it's not a child, but the dir itself
-		if (strcmp(pd_child->d_name, DOTDIR) == 0) {
-			// get the few pieces of information that we care about from the `dirent` object
-			// note: we're not keeping `d_name` or `d_namlen`, since these would just
-			//	return "." and 1 respectively, which isn't much help to us
-			p_fsobj->type = pd_child->d_type;
-			p_fsobj->inum = pd_child->d_ino;
-
-			continue; // continue - the rest of the information we need is already in `p_fsobj->s`
-		}
-
-		// with the output structure I'm building, I don't think it makes sense to show `..`
-		//	if I want to add an option to keep it later, I can just add it to this condition
-		if (strcmp(pd_child->d_name, "..") == 0) continue;
-		// note: when I add the `-a` and `-A` options later, this is where I'll add a check for them
-
-		/* —— – alloc mem for children —————————————————————————————————— */
-
-		// keep track of the number of children in the dir
-		if (p_fsobj->f->child_count >= child_alloc_count) {
-			const int32_t old_alloc_count = child_alloc_count; // save the old capacity before multiplying
-
-			// if the number of children (before we increment it) is more than we have space for,
-			//	then increase the number of children we have space for, by a factor of 1.5.
-			// 1.5 is the optimal multiplier to make sure we don't over-allocate memory, but also so that
-			//	we aren't constantly allocating it either
-			MULT_BY_1_5(child_alloc_count);
-
-			// re-allocate the memory. note that `[e]realloc` will free the original pointer and hard exit if it
-			//	fails, so we don't have to worry about memory leaks by overwriting the original pointer
-			children = erealloc(children, sizeof(FileStat) * child_alloc_count);
-
-			// zero out the new memory that we were just allocated
-			//	this isn't strictly necessary, but it could prove rly useful for debugging
-			//	(I might remove it later if I don't think it's needed)
-			memset(children + old_alloc_count, 0, sizeof(FileStat) * (child_alloc_count - old_alloc_count));
-		}
-
-		// find the position where the child's `FileStat` object will start
-		FileStat *pfs_child = children + p_fsobj->f->child_count;
-
-		// only increment the child count now that we've used it as an index to calculate where the child should be
-		p_fsobj->f->child_count++;
-
-		processChild(pfs_child, pd_child, dirpath, dirpath_len);
-	}
-
-	closedir(p_dir);
-	return p_fsobj;
+	// if the input was a directory, however, we need to find & process its children
+	return processDir(path, p_fsobj);
 }
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
