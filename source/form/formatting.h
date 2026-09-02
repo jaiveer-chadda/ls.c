829876d30d967a86316b90a174363371789466d7
JV <jaiveer.chadda@gmail.com>
2026-09-02 16:26:54 +0400

    feat: Remake `formatting.h`/`init-formatting.c` to be more condensed and adaptable

--- source/form/formatting.h
+++ source/form/formatting.h
@@ -10,81 +10,37 @@ void initFormatting(void);
 /* ——————————————————————————————————————————————————————— */
 
 typedef struct {
-	size_t	nlink	;
-	size_t	dev_no	;	size_t	inode  ;
+	const char title[8];
+	const char fmt_s[6];
+	const char fmt_l[6];
+	size_t len;
+} field_t;
 
-	size_t	flags	;	size_t flag_str;
-	size_t	mode	;	size_t mode_str;
-	size_t	size	;	size_t size_str;
-	size_t	uid		;	size_t usr_name;
-	size_t	gid		;	size_t grp_name;
-	size_t	time	;	size_t time_str;
-} Lengths;
+extern field_t fields[];
 
-extern Lengths field_lengths;
+#define FIELDS_TABLE \
+	/* field		  title		  fmt_s	  fmt_l	   */ \
+	X(FI_name		, "Name"	, "%s"	, "%*s"		) \
+	X(FI_nlink		, "Links"	, "%d"	, "%*d"		) \
+	X(FI_dev_no		, "Dev No"	, "%d"	, "%*d"		) \
+	X(FI_inode		, "Inode"	, "%llu", "%*llu"	) \
+	X(FI_flags		, "Flags"	, "%x"	, "%*x"		) \
+	X(FI_flag_str	, "Flags"	, "%s"	, "%-*s"	) \
+	X(FI_mode		, "Mode"	, "%o"	, "%0*o"	) \
+	X(FI_mode_str	, "Perms"	, "%s"	, "%-*s"	) \
+	X(FI_size		, "Size"	, "%zd"	, "%*zd"	) \
+	X(FI_size_str	, "Size"	, "%s"	, "%*s"		) \
+	X(FI_uid		, "UID"		, "%d"	, "%*d"		) \
+	X(FI_usr_name	, "User"	, "%s"	, "%-*s"	) \
+	X(FI_gid		, "GID"		, "%d"	, "%*d"		) \
+	X(FI_grp_name	, "Group"	, "%s"	, "%-*s"	) \
+	X(FI_time		, "Time"	, "%ld"	, "%*ld"	) \
+	X(FI_time_str	, "Time"	, "%s"	, "%*s"		) \
+	X(FI_majmin		, "Size"	, "%#x"	, "%#*x"	) \
 
-/* ——————————————————————————————————————————————————————— */
-
-typedef char fmt_spec[6];
-
-typedef struct {
-	fmt_spec nlink	;
-	fmt_spec dev_no	;	fmt_spec inode	;
-
-	fmt_spec flags	;	fmt_spec flag_str;
-	fmt_spec mode	;	fmt_spec mode_str;
-	fmt_spec size	;	fmt_spec size_str;
-	fmt_spec majmin	;
-	fmt_spec uid	;	fmt_spec usr_name;
-	fmt_spec gid	;	fmt_spec grp_name;
-	fmt_spec time	;	fmt_spec time_str;
-} FmtStrs;
-
-extern FmtStrs fmt_strs_short, fmt_strs_long;
-
-/* ——————————————————————————————————————————————————————— */
-
-extern const bool
-	NAME_MIN_LEN,
-	SUFFIX_MIN_LEN,
-	LINK_TO_MIN_LEN,
-
-	NLINK_MIN_LEN,
-	DEV_NO_MIN_LEN,
-	INODE_MIN_LEN,
-
-	FLAGS_MIN_LEN,	FLAG_STR_MIN_LEN,
-	MODE_MIN_LEN,	MODE_STR_MIN_LEN,
-	SIZE_MIN_LEN,	SIZE_STR_MIN_LEN,
-	UID_MIN_LEN,	USR_NAME_MIN_LEN,
-	GID_MIN_LEN,	GRP_NAME_MIN_LEN,
-	TIME_MIN_LEN,	TIME_STR_MIN_LEN;
-
-/* ——————————————————————————————————————————————————————— */
-
-#define name_TITLE		"Name"
-
-#define nlink_TITLE		"Links"
-#define dev_no_TITLE	"Dev No"
-#define inode_TITLE		"Inode"
-
-#define flags_TITLE		"Flags"
-#define flag_str_TITLE	"Flags"
-
-#define mode_TITLE		"Mode"
-#define mode_str_TITLE	"Perms"
-
-#define size_TITLE		"Size"
-#define size_str_TITLE	"Size"
-
-#define uid_TITLE		"UID"
-#define usr_name_TITLE	"User"
-
-#define gid_TITLE		"GID"
-#define grp_name_TITLE	"Group"
-
-#define time_TITLE		"MTime"
-#define time_str_TITLE	"Modified"
+#define X(field, title, fmt_s, fmt_l) field,
+typedef enum { FIELDS_TABLE FI_COUNT } Field;
+#undef X
 
 /* ——————————————————————————————————————————————————————— */
 
--- source/form/init-formatting.c
+++ source/form/init-formatting.c
@@ -5,48 +5,16 @@
 #include "formatting.h"
 #include "options/options.h"
 
-Lengths field_lengths;
-FmtStrs fmt_strs_short, fmt_strs_long;
+#define X(fld, ttl, fms, fml) \
+	[fld] = (field_t){ .title = ttl, .fmt_s = fms, .fmt_l = fml, .len = 0 },
+
+field_t fields[FI_COUNT] = { FIELDS_TABLE };
+#undef X
 
 inline void initFormatting(void) {
 	if (DO_HEADER()) {
-		// these will be precomputed when compiler optimisation is set to `-O2` or above
-		field_lengths = (Lengths){
-			.nlink	= strlen(nlink_TITLE),
-			.dev_no	= strlen(dev_no_TITLE),	.inode	  =	strlen(inode_TITLE),
-			// `mode` and `mode_str` have constant sizes
-			.mode	= OCT_MODE_LEN,			.mode_str =	MODE_STR_LEN - 1, // -1 to disregard the nullbyte
-			.flags	= strlen(flags_TITLE),	.flag_str =	strlen(flag_str_TITLE),
-			.size	= strlen(size_TITLE),	.size_str =	strlen(size_str_TITLE),
-			.uid	= strlen(uid_TITLE),	.usr_name =	strlen(usr_name_TITLE),
-			.gid	= strlen(gid_TITLE),	.grp_name =	strlen(grp_name_TITLE),
-			.time	= strlen(time_TITLE),	.time_str =	strlen(time_str_TITLE),
-		};
+		#define X(fld, ttl, fms, fml) fields[fld].len = sizeof(ttl);
+		FIELDS_TABLE
+		#undef X
 	}
-
-	fmt_strs_short = (FmtStrs){
-		.nlink	= "%d"	,
-		.dev_no	= "%d"	, .inode	= "%llu",
-
-		.flags	= "%x"	, .flag_str	= "%s",
-		.mode	= "%o"	, .mode_str	= "%s",
-		.size	= "%zd"	, .size_str	= "%s",
-		.majmin	= "%#x"	,
-		.uid	= "%d"	, .usr_name	= "%s",
-		.gid	= "%d"	, .grp_name	= "%s",
-		.time	= "%ld"	, .time_str	= "%s",
-	};
-
-	fmt_strs_long = (FmtStrs){
-		.nlink	= "%*d"	,
-		.dev_no	= "%*d"	, .inode	= "%*llu",
-
-		.flags	= "%*x"	, .flag_str	= "%-*s",
-		.mode	= "%0*o", .mode_str	= "%-*s",
-		.size	= "%*zd", .size_str	= "%*s",
-		.majmin	= "%#*x",
-		.uid	= "%*d"	, .usr_name	= "%-*s",
-		.gid	= "%*d"	, .grp_name	= "%-*s",
-		.time	= "%*ld", .time_str	= "%*s",
-	};
 }
