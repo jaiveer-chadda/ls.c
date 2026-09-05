e8372dccb4c0135b9263ef5d1852d129729d5f75
JV <jaiveer.chadda@gmail.com>
2026-09-05 18:51:42 +0400

    refactor(output): Move the printing of the main fields into its own function.
    Also, comment the rest of the file which has, for now, been left undocumented

--- source/output/print-file.c
+++ source/output/print-file.c
@@ -8,37 +8,32 @@
 #include "options/options.h"
 #include "features/features.h"
 
+static inline void printFields(const FileStat *const pFS);
+
+/* —— printFile() —————————————————————————————————————————————————————————————————————————————————————————————————— */
+
 void printFile(const FileStat *const pFS, const uint8_t depth, const bool is_last, const lines_t lines) {
 	// if an file's `FileStat` pointer points to `NULL`, we weren't able to be `stat` it in the first place
 	if (pFS == NULL) return;
 
-	/* —— print main fields ——————————————————————————————————————————————————————————————————————————— */
-
-	colprint(RESET_ALL);
-
-	if (do_inum		()) print_inum	  (pFS);
-	if (do_mode		()) print_mode	  (pFS);
-	if (do_mode_str	()) print_mode_str(pFS);
-	if (do_size_str	()) print_size_str(pFS);
-	if (do_usr_name	()) print_usr_name(pFS);
-	if (do_grp_name	()) print_grp_name(pFS);
-	if (do_flag_str	()) print_flag_str(pFS);
-	if (do_time_str ()) {
-		if (do_time_t(A_TIME)) print_time_str(pFS, A_TIME);
-		if (do_time_t(M_TIME)) print_time_str(pFS, M_TIME);
-		if (do_time_t(C_TIME)) print_time_str(pFS, C_TIME);
-		if (do_time_t(B_TIME)) print_time_str(pFS, B_TIME);
-	}
-
-	/* —— print name & extras ————————————————————————————————————————————————————————————————————————— */
-
 	lines_t new_lines = {0};
 
+	/* —— print fields ———————————————————————————————————————————————————————————————————————————————— */
+
+	// make sure we're not starting the line with any colour leaking
+	colprint(RESET_ALL);
+
+	// print all of the main feature fields
+	printFields(pFS);
+
+	// print the tree branches before the name
 	print_tree(new_lines, lines, depth, is_last);
+	// print the file's icon and name
 	print_name(pFS);
 
+	// finally, print the file's suffix and end the file's output by outputting a newline
 	if (do_suffix() && pFS->suffix != '\0') putchar(pFS->suffix);
-	putchar('\n');  // end the entry printing, by outputting a newline
+	putchar('\n');
 
 	/* —— error checking —————————————————————————————————————————————————————————————————————————————— */
 
@@ -56,7 +51,39 @@ void printFile(const FileStat *const pFS, const uint8_t depth, const bool is_las
 
 	/* —— recurse ————————————————————————————————————————————————————————————————————————————————————— */
 
+	// if we made it through the checks, then iterate through this dir's children, and recursively print them
 	for (int i = 0; i < pFS->f->child_count; i++) {
 		printFile(&pFS->f->children[i], depth + 1, i == pFS->f->child_count - 1, new_lines);
 	}
 }
+
+/* —— printFields() ———————————————————————————————————————————————————————————————————————————————————————————————— */
+
+#define print_time_raw(p,t) /** @todo implement */;
+
+// note: to use the `print_field` macro, a function must have the following signature:
+//	`void print_[field_name](const FileStat *const pFS)`
+#define print_field(field)  if (do_##field()) print_##field(pFS)
+#define print_time(type) if (do_time_t(type)) { \
+	if (do_time		()) print_time_raw(pFS, (type)); \
+	if (do_time_str	()) print_time_str(pFS, (type)); \
+}
+
+/* ——————————————————————————————————————————————————— */
+
+static inline void printFields(const FileStat *const pFS) {
+	print_field(inum	);
+	print_field(mode	);
+	print_field(mode_str);
+	print_field(size_str);
+	print_field(usr_name);
+	print_field(grp_name);
+	print_field(flag_str);
+
+	print_time(A_TIME);
+	print_time(M_TIME);
+	print_time(C_TIME);
+	print_time(B_TIME);
+}
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
