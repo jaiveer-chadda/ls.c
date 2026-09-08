aa60b68789a12440de55fbe8d9483f017b70ff33
JV <jaiveer.chadda@gmail.com>
2026-09-08 13:44:18 +0400

    fix(output): Completely rework `printEscdName` so that it (finally finally) prints absolutely no extra characters or escape sequences, while still correctly escaping everything that needs to be escaped.
    Everything's very very messy right now, but I'm committing here cos it's (as far as I can tell), 100% working. I'll clean it up and refactor in a moment.

--- source/output/escape-name.c
+++ source/output/escape-name.c
@@ -7,15 +7,29 @@
 #include "options/options.h"
 #include "graphics/graphics.h"
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
-#define DO_OCT_ESC(chr) ( 0 < (chr) && (chr) <= 7 )
-#define DO_HEX_ESC(chr) ((7 < (chr) && (chr) <= 31) || (chr) == 127)
+#define DO_OCT_ESC(chr) ( 0 < (chr) && (chr) <= 7 ) /** 1 → 7 */
+#define DO_HEX_ESC(chr) ((7 < (chr) && (chr) <= 31) || (chr) == 127) /** 8 → 31, 127 */
+
+/** 1 → 31, 92, 127 */
+#define DO_ANY_ESC(chr) \
+	(DO_OCT_ESC(chr) || DO_HEX_ESC(chr) || (chr) == '\\')
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
-#define copy_and_return(fmt, src) \
-	return sprintf(esc, ("%s\\" fmt "%s"), IFCOLOUR(char_ansi), (src), IFCOLOUR(file_ansi))
+#define copy_and_return(fmt, src) do { \
+	if (last_was_esc) { \
+		return sprintf(esc, ("\\" fmt), (src)); \
+	} else { \
+		return sprintf(esc, ("%s\\" fmt /* "%s" */), IFCOLOUR(char_ansi), (src)/* , IFCOLOUR(file_ansi) */); \
+	} \
+} while (0)
 
 /**
  * @brief Find the appropriate escape sequence for an inputted character.
@@ -27,7 +41,8 @@
 static inline uint8_t escapeCharacter(
 	char *esc, const char inp,
 	const char *const file_ansi,
-	const char *const char_ansi
+	const char *const char_ansi,
+	const bool last_was_esc
 ) {
 	switch (inp) {
 		case '\\' : copy_and_return("%c", '\\');
@@ -45,6 +60,9 @@ static inline uint8_t escapeCharacter(
 	if DO_OCT_ESC(inp) copy_and_return("%hu"  , (uint8_t)inp);
 	if DO_HEX_ESC(inp) copy_and_return("x%02X", (uint8_t)inp);
 
+	/* —————————————————————————————————————————————————————————————————— */
+	// nothing to escape
+
 	*esc = inp;
 	return 1;
 }
@@ -52,34 +70,112 @@ static inline uint8_t escapeCharacter(
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
 void printEscdName(const char *const name, const namlen_t name_len, const Colour colour) {
-	// if the colour has a background, use the background escape instead
-	const char *const esc_ansi = colour.has_bg() ? ESC_CHAR_BG_ANSI	  : ESC_CHAR_FG_ANSI;
-	const Colour	esc_colour = colour.has_bg() ? ESC_CHAR_BG_COLOUR : ESC_CHAR_FG_COLOUR;
+	(void)name; (void)name_len; (void)colour;
 
-	const Colour save_colour = getActive();
+	/*
+	 * if
+	 * 
+	 */
+
+	/* —————————————————————————————————————————————————————————————————— */
+
+	fputs(PRE_NAME_PAD, stdout);
+
+	const char *const esc_ansi = colour.has_bg() ? ESC_CHAR_BG_ANSI : ESC_CHAR_FG_ANSI;
+	const Colour esc_colour = colour.has_bg() ? ESC_CHAR_BG_COLOUR : ESC_CHAR_FG_COLOUR;
+	const Colour prev_active_col = getActive();
+
+	uint8_t init_ansi_len, file_ansi_len;
+
+	char *getcol_ptr = getcol_ns_len(colour, &init_ansi_len);
+	char init_ansi[MAX_ANSI_SIZE];
+	memcpy(init_ansi, getcol_ptr, init_ansi_len + 1);
+
+	// make `getcol` think that it's printing after an escape sequence, and see what it returns
 	setActive(esc_colour);
-
-	uint8_t file_ansi_len = 0;
 	const char *const file_ansi = getcol_ns_len(colour, &file_ansi_len);
-	setActive(save_colour);
+	setActive(prev_active_col);
 
-	// the exact size is temporary for now - it'll be determined dynamically later
+	// allocate memory for the output, and setup the output pointer
 	char *const output = emalloc(name_len + 1024);
 	char *out_ptr = output;
 
-	memcpy(out_ptr, file_ansi, file_ansi_len);
-	out_ptr += file_ansi_len;
+	// if the file's colour is already the one that's active, then we don't have to print anything
+	//	otherwise, add the file's colour to the start of the output sequence
 
-	for (const char *inp_ptr = name; *inp_ptr != '\0'; inp_ptr++) {
-		out_ptr += escapeCharacter(out_ptr, *inp_ptr, file_ansi, esc_ansi);
+	if (!areEqual(colour, prev_active_col) && !DO_ANY_ESC(name[0])) {
+		memcpy(out_ptr, init_ansi, init_ansi_len + 1);
+		out_ptr += init_ansi_len;
+	} else {
+		// fputs("<s>", stdout); // (same)
 	}
 
-	if (DO_COLOUR()) memcpy(out_ptr, RESET, sizeof(RESET));
+	uint8_t esc_len = 0;
+	bool last_was_esc = false, did_esc = false, is_first;
+	(void)did_esc;
 
-	printf("%s%s", PRE_NAME_PAD, output);
+	const char *inp_ptr;
 
-	setActive(RESET_ALL);
+	for (inp_ptr = name; *inp_ptr != '\0'; inp_ptr++) {
+
+		out_ptr += ( esc_len = escapeCharacter(out_ptr, *inp_ptr, file_ansi, esc_ansi, DO_ANY_ESC(*(inp_ptr - 1))) );
+
+		// if this char was escaped, but the next one won't be
+		if (DO_ANY_ESC(*inp_ptr) && !DO_ANY_ESC(*(inp_ptr + 1)) && *(inp_ptr + 1) != '\0') {
+			memcpy(out_ptr, file_ansi, file_ansi_len);
+			out_ptr += file_ansi_len;
+		}
+	}
+
+	*out_ptr = '\0';
+
+	fputs(output, stdout);
+	// printf("<|>");
+
+	if (DO_ANY_ESC(*(inp_ptr - 1))) {
+		setActive(esc_colour);
+	} else {
+		setActive(colour);
+	}
+
+	// Colour temp = getActive();
+
+	// setActive(RESET_ALL);
+	// printf(" << act = %s >> ", getcol(temp));
+
 	efree(output);
+	return;
+
+	/* —————————————————————————————————————————————————————————————————— */
+	/* —————————————————————————————————————————————————————————————————— */
+
+
+	// const Colour save_colour = getActive();
+	// setActive(colour.has_bg() ? ESC_CHAR_BG_COLOUR : ESC_CHAR_FG_COLOUR);
+
+	// uint8_t file_ansi_len = 0;
+	// const char *const file_ansi = getcol_ns_len(colour, &file_ansi_len);
+	// setActive(save_colour);
+
+	// // the exact size is temporary for now - it'll be determined dynamically later
+	// char *const output = emalloc(name_len + 1024);
+	// char *out_ptr = output;
+
+	// memcpy(out_ptr, file_ansi, file_ansi_len);
+	// out_ptr += file_ansi_len;
+
+	// for (const char *inp_ptr = name; *inp_ptr != '\0'; inp_ptr++) {
+	// 	out_ptr += escapeCharacter(out_ptr, *inp_ptr, file_ansi, esc_ansi);
+	// }
+
+	// if (DO_COLOUR()) memcpy(out_ptr, RESET, sizeof(RESET));
+
+	// printf("%s%s", PRE_NAME_PAD, output);
+
+	// setActive(RESET_ALL);
+	// efree(output);
 }
+
+#pragma clang diagnostic pop
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
