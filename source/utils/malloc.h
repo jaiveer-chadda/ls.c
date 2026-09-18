82b17b7961f38b20ac7a483cdcff7109a8605793
JV <jaiveer.chadda@gmail.com>
2026-09-18 09:46:42 +0400

    feat(utils): Set up the debug/non-debug macros to handle the custom malloc/free functions and their parameters.

--- source/utils/malloc.h
+++ source/utils/malloc.h
@@ -1,24 +1,43 @@
 /// @file utils/malloc.h
 
-#ifndef MALLOC_INITIALISED
-#define MALLOC_INITIALISED
+#ifndef UTILS__MALLOC_H
+#define UTILS__MALLOC_H
 
 #include <stdlib.h>
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
 #ifdef DEBUG_MODE
-	void *e__malloc(size_t size);
-	void *e__calloc(size_t count, size_t size);
-	void *e__realloc(void *ptr, size_t size);
-	void  e__free(void *ptr);
+#	define E__DEBUG_ARGS		\
+		const char *const file,	\
+		const char *const func,	\
+		const int lineno
+
+#	define E__DEBUG_PARAMS \
+		__FILE__, __func__, __LINE__
+
+	void *e__malloc	(E__DEBUG_ARGS, size_t size);
+	void *e__calloc	(E__DEBUG_ARGS, size_t count, size_t size);
+	void *e__realloc(E__DEBUG_ARGS, void *ptr, size_t size);
+	void  e__free	(E__DEBUG_ARGS, void *ptr);
 	void  e__checkMemLeak(void);
+
+#	define emalloc(size			) e__malloc	(E__DEBUG_PARAMS, size		 )
+#	define ecalloc(count, size	) e__calloc	(E__DEBUG_PARAMS, count, size)
+#	define erealloc(ptr	, size	) e__realloc(E__DEBUG_PARAMS, ptr  , size)
+#	define efree(ptr			) e__free	(E__DEBUG_PARAMS, ptr		 )
 #	define checkMemLeak() e__checkMemLeak()
+
 #else
 	void *e__malloc(size_t size);
 	void *e__calloc(size_t count, size_t size);
 	void *e__realloc(void *ptr, size_t size);
 	void  e__free(void *ptr);
+
+#	define emalloc(size			) e__malloc	(size		)
+#	define ecalloc(count, size	) e__calloc	(count, size)
+#	define erealloc(ptr	, size	) e__realloc(ptr  , size)
+#	define efree(ptr			) e__free	(ptr		)
 #	define checkMemLeak()
 #endif
 
@@ -34,4 +53,4 @@
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
-#endif /* !MALLOC_INITIALISED */
+#endif /* !UTILS__MALLOC_H */
